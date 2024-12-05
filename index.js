411f72f232bb38c1100e95237b28b0b220a6135c
BatiBati <94386708+BatiBati@users.noreply.github.com>
2024-12-05 18:11:53 +0800

    Exercise 2 hiij bga

--- index.js
+++ index.js
@@ -55,9 +55,16 @@ let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
 Example output:
 276 + 351 = 627
  */
-
-
-
+let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2];
+let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26];
+for (let i = 0; i < arr_1.length; i++) {
+  let sumOfFirstarray = 0;
+  let sumOfSecondarray = 0;
+  sumOfFirstarray = sumOfFirstarray + arr_1 [i];
+  sumOfSecondarray = sumOfSecondarray + arr_2 [i];
+  console.log(i);
+  
+}
 
 /*
 Exercise 4
